8d57cd247b4b8b866e874d0220b339f70b85fb42
Tahsin <tahsin@pop-os.localdomain>
2024-05-14 11:59:40 -0400

    Move camera with mouse

--- Assets/Scripts/FPSCamera.cs
+++ Assets/Scripts/FPSCamera.cs
@@ -1,18 +1,37 @@
+// Camera movement based on tutorial by Unity Ace: https://www.youtube.com/watch?v=5Rq8A4H6Nzw
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FPSCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float mouseSens = 2f;
+    float cameraHRot = 0f;
+    float cameraVRot = 0f;
+    
+    bool lockedCursor = true;
+
+
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
+ 
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float inputX = Input.GetAxis("Mouse X") * mouseSens;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSens;
+
+        cameraVRot -= inputY;
+        cameraVRot = Mathf.Clamp(cameraVRot, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVRot;
+
+        cameraHRot += inputX;
+        cameraHRot = Mathf.Clamp(cameraHRot, -45f, 45f);
+        transform.localEulerAngles += Vector3.up * cameraHRot;
+
     }
 }
