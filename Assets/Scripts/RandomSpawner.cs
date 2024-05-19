0b16733634f1894f5ba53387c0bbd51e0906de10
Tahsin <tahsin@pop-os.localdomain>
2024-05-19 00:18:12 -0400

    Move target when shot

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
 {
+    public static RandomSpawner instance;
 
     public GameObject targetPrefab;
+    private static GameObject target;
 
-    // Update is called once per frame
-    void Update()
+    
+    private void Awake()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Instantiate(targetPrefab, transform.position, Quaternion.identity);
-        }
+        instance = this;
+    }
+
+    void Start()
+    {
+        target = Instantiate(targetPrefab, transform.position, Quaternion.identity);
+    }
+
+
+    public void AddPoint()
+    {
+        // add a point to the counter
+        // move the target to another location
+        Vector3 randomDisplacement = new Vector3(Random.Range(-40f, 40f), Random.Range(-40f, 40f), 0);
+        target.transform.SetLocalPositionAndRotation(transform.position + randomDisplacement, Quaternion.identity);
     }
 }
--- Assets/Scripts/TargetPrefab.cs
+++ Assets/Scripts/TargetPrefab.cs
@@ -5,6 +5,6 @@ using UnityEngine;
 public class TargetPrefab : MonoBehaviour
 {
     public void Damage(){
-
+        RandomSpawner.instance.AddPoint();
     }
 }
